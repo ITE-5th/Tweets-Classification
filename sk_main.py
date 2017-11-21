f2096c7fca23f1793acbbdcff66949188eedc1ce
Obada <obada.jabassini@gmail.com>
2017-11-21 19:38:15 +0200

    Drop duplicates

--- sk_main.py
+++ sk_main.py
@@ -18,6 +18,7 @@ from transformers.words_count_transformer import WordsCountTransformer
 
 
 def pre_process(data):
+    data.drop_duplicates(subset="tweet", inplace=True)
     data["sentiment"] = data["sentiment"].apply(lambda x: int(x.strip() == "yes"))
     data["tweet"] = data["tweet"].apply(lambda x: x.strip())
     data["tweet"] = data["tweet"].apply(lambda x: strip_tashkeel(x))

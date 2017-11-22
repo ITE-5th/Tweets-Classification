143e7809e824b7b9252e672428a90c9e45671af1
Obada <obada.jabassini@gmail.com>
2017-11-22 05:39:16 +0200

    What ever

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -18,6 +18,9 @@ def pre_process_tweet(data):
     data.loc[:, "tweet"] = data.loc[:, "tweet"].apply(lambda x: re.sub("[أإآ]", "ا", x))
     data.loc[:, "tweet"] = data.loc[:, "tweet"].apply(lambda x: re.sub("#", "", x))
     data.loc[:, "tweet"] = data.loc[:, "tweet"].apply(lambda x: re.sub("_", " ", x))
+    data.loc[:, "tweet"] = data.loc[:, "tweet"].apply(lambda x: re.sub(r'(.)\1+', r'\1', x))
+    data.loc[:, "tweet"] = data.loc[:, "tweet"].apply(
+        lambda x: re.sub("([1-9] |1[0-9]| 2[0-9]|3[0-1])(.|-)([1-9] |1[0-2])(.|-|)(20|14)[0-9][0-9]", " ", x))
     # TODO: are we sure that we should delete all the tweets with any english letter?
     # data = data.loc[~data.loc[:, "tweet"].str.contains("[a-zA-Z]"), :]
     data = data.loc[~data.loc[:, "tweet"].str.contains("http"), :]
